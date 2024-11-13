920a473cb56a4bd54398d8ba900dc0ab97e6ca7e
samarth-0205 <samarth022005@gmail.com>
2024-11-13 17:03:23 +0530

    01.js & datatype.js & operator.js files

---
+++ 01.js
@@ -0,0 +1,21 @@
+// Structured programming Launguage
+// Scripting Language
+
+// print statement
+
+console.log("Hello World")
+
+// variables
+
+// Let, const, var
+
+let age = 20
+const price = 222.20
+var name = "abc"
+
+console.log(age);
+console.log(typeof(age));
+console.log(price);
+console.log(typeof(price));
+console.log(name);
+console.log(typeof(name));
