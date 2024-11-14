91a021a37716dfb1175aab4bad7e3cc826c7632e
samarth-0205 <samarth022005@gmail.com>
2024-11-14 16:40:10 +0530

    Add files via upload

---
+++ javacript_practice/dt_practice.js
@@ -0,0 +1,32 @@
+let name = "samarth"
+
+let myage = 19
+
+let todayiswednesday = true
+
+let rupees = null
+
+let anyproduct
+
+let company = ["apple","samsung","oppo",22,33.89,false]
+
+let details = {
+    name: "Thor",
+    age: 25,
+    city: "Los Angeles"
+}
+
+console.log(name);
+console.log(typeof(name));
+console.log(myage);
+console.log(typeof(myage));
+console.log(todayiswednesday);
+console.log(typeof(todayiswednesday));
+console.log(rupees);
+console.log(typeof(rupees));
+console.log(anyproduct);
+console.log(typeof(anyproduct));
+console.log(company);
+console.log(typeof(company));
+console.log(details);
+console.log(typeof(details));
