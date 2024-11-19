2471d29cc024319ef010adc553111a4cf4a443d1
samarth-0205 <samarth022005@gmail.com>
2024-11-19 15:17:33 +0530

    operator

---
+++ javacript_practice/operator.js
@@ -0,0 +1,32 @@
+//Operators:
+
+// Arithmetic Operators: +, -, *, /, %
+// Assignment Operators: +=, -=, *=, /=, %=
+// Relational Operators: ==, !=, !==, ===
+// Logical Operators: and, or, not
+// Bitwise Operators: &, |, ^, ~, <<, >>
+// Increment & Decrement Operators: ++, --
+
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+
+// console.log(a+=b);
+// console.log(a-=b);
+// console.log(a*=b);
+// console.log(a/=b);
+// console.log(a%=b);
+
+// console.log(a == b); // value
+// console.log(a != b); // value
+// console.log(a === b); // value & datatype
+// console.log(a !== b); // value & datatype
+
+let a = 10, b = 5;
+console.log(--a);
+// console.log(a);
+console.log(++b);
+// console.log(b);
