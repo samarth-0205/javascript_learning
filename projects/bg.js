4c3a2243881f2a12a25df0a517b91b8de9c9d47f
samarth-0205 <samarth022005@gmail.com>
2024-11-19 15:26:58 +0530

    bg

---
+++ projects/bg.js
@@ -0,0 +1,11 @@
+document.getElementById('redBtn').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'red';
+});
+
+document.getElementById('blueBtn').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'blue';
+});
+
+document.getElementById('yellowBtn').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'yellow';
+});
