32c13bd7a98b1cbac06ba98b1d49444187410822
samarth-0205 <samarth022005@gmail.com>
2024-11-20 14:24:54 +0530

    datatypes

---
+++ learning/datatypes.js
@@ -0,0 +1,51 @@
+// JavaScript has 8 Datatypes
+
+// String
+let class_name = "12th"
+
+// Number
+let age = 19
+
+// Boolean
+let isPresent = false
+
+// Null
+let price = null
+
+// Undefined
+let name
+
+// Array
+let cars = ["bmw", "tata", "audi", 15, 55.222, true]
+
+// Object
+let person = {
+    "name": "abc",
+    "age": 55,
+    "mobile_no": "4565214568",
+    "email": "abc@gmail.com"
+}
+
+
+// console.log(class_name);
+// console.log(typeof(class_name));
+// console.log(age);
+// console.log(typeof(age));
+// console.log(isPresent);
+// console.log(typeof(isPresent));
+// console.log(price);
+// console.log(typeof(price));
+// console.log(name);
+// console.log(typeof(name));
+// console.log(cars);
+// console.log(typeof(cars));
+// console.log(person);
+// console.log(typeof(person));
+
+
+// Symbol
+
+
+let sum = 100 + 1 + 4 + "2" + 8 + 2
+
+console.log(sum);
